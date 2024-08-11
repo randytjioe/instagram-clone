2af00ef4e6954599c1d1a61a42e64b14795e165d
randy <randytjiioe95@gmail.com>
2024-08-12 00:33:59 +0700

    edit header

--- App.js
+++ App.js
@@ -282,7 +282,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 50,
+    paddingTop: 30,
   },
   header: {
     flexDirection: "row",
